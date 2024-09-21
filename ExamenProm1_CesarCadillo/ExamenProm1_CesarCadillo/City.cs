c9ffdd3359c36fe05783c9c64942621056802008
César Cadillo <154623288+CesarCadillo@users.noreply.github.com>
2024-09-20 22:01:19 -0500

    Interface IBuilding / Abstract class Building / Update & syntax corrections City.cs

--- ExamenProm1_CesarCadillo/ExamenProm1_CesarCadillo/City.cs
+++ ExamenProm1_CesarCadillo/ExamenProm1_CesarCadillo/City.cs
@@ -8,17 +8,19 @@ namespace ExamenProm1_CesarCadillo
 {
     public class City
     {
-        public string name { get; set; }
-        public int money { get; set; }
-        public int population { get; set; }
-        public int currentMonth { get; set; }
+        public string Name { get; set; }
+        public int Money { get; set; }
+        public int Population { get; set; }
+        public int CurrentMonth { get; set; }
+        public List<IBuilding> Buildings { get; set; }
 
         public City(string name, int money, int population, int currentMonth)
         {
-            this.name = name;
-            this.money = money;
-            this.population = population;
-            this.currentMonth = currentMonth;
+            Name = name;
+            Money = money;
+            Population = population;
+            CurrentMonth = currentMonth;
+            Buildings = new List<IBuilding>();            
         }
 
     }
